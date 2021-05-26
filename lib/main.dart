6f87e6eaac020151b64c476a0f7901a1318795dc
thomasbonnet12 <thomas.bonnet.12@gmail.com>
2021-05-26 22:21:16 +0200

    splash screen and boiler plate created!

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cat_dog_ai_classifier/splashscreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,6 +9,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: 'Cat and Dog AI Classifier',
+      home: MySplash(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
