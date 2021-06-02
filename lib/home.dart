9c04d75166c2327c1a4eaa8ccd62bb738f8f1ee7
thomasbonnet12 <thomas.bonnet.12@gmail.com>
2021-06-02 21:22:23 +0200

    home screen UI boiler plate

--- lib/home.dart
+++ lib/home.dart
@@ -6,11 +6,47 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool _loading = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(),
+      backgroundColor: Colors.white,
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 50),
+            Text(
+              'Coding AI',
+              style: TextStyle(color: Colors.black, fontSize: 20),
+            ),
+            SizedBox(height: 5),
+            Text(
+              'Cats and Dogs AI detector app',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 30),
+            ),
+            SizedBox(height: 50),
+            Center(
+              child: _loading
+                  ? Container(
+                      child: Column(
+                        children: [
+                          Image.asset('assets/cat_dog_icon.png'),
+                          SizedBox(height: 50),
+                        ],
+                      ),
+                    )
+                  : Container(),
+            )
+          ],
+        ),
+      ),
       bottomNavigationBar: BottomAppBar(),
     );
   }
