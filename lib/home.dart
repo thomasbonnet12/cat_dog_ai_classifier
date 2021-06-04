cf639f9a5bb89e6e568718c37208e6be3b576140
thomasbonnet12 <thomas.bonnet.12@gmail.com>
2021-06-04 22:34:07 +0200

    final touches!

--- lib/home.dart
+++ lib/home.dart
@@ -67,110 +67,115 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blueAccent,
-      ),
+      //appBar: AppBar(
+      //backgroundColor: Colors.blueAccent,
+      //),
       backgroundColor: Colors.blueAccent,
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 24),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 50),
-              Text(
-                'Coding AI',
-                style: TextStyle(color: Colors.white, fontSize: 20),
-              ),
-              SizedBox(height: 5),
-              Text(
-                'Cats and Dogs AI detector app',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 30),
-              ),
-              SizedBox(height: 30),
-              Center(
-                child: _loading
-                    ? Container(
-                        width: 350,
-                        child: Column(
-                          children: [
-                            Image.asset('assets/cat_dog_icon.png'),
-                            SizedBox(height: 10),
-                          ],
-                        ),
-                      )
-                    : Container(
-                        child: Column(
-                          children: [
-                            Container(
-                              height: 250,
-                              child: Image.file(_image),
-                            ),
-                            SizedBox(height: 20),
-                            _output != null
-                                ? Text(
-                                    'Hurray !! We found out this is a ${_output[0]['label']}',
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 15),
-                                  )
-                                : Container(),
-                            SizedBox(height: 10),
-                          ],
-                        ),
-                      ),
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                child: Column(
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        pickImage();
-                      },
-                      child: Container(
-                        width: MediaQuery.of(context).size.width - 150,
-                        alignment: Alignment.center,
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(15)),
-                        child: Text(
-                          'Capture Image',
-                          style: TextStyle(color: Colors.black),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 12),
-                    GestureDetector(
-                      onTap: () {
-                        pickGalleryImage();
-                      },
-                      child: Container(
-                        width: MediaQuery.of(context).size.width - 150,
-                        alignment: Alignment.center,
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(15)),
-                        child: Text(
-                          'Select an Image',
-                          style: TextStyle(color: Colors.black),
-                        ),
-                      ),
-                    ),
-                  ],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 50),
+                Text(
+                  'Coding AI',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
                 ),
-              ),
-            ],
+                SizedBox(height: 5),
+                Text(
+                  'Cats and Dogs AI detector app',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 30),
+                ),
+                SizedBox(height: 30),
+                Center(
+                  child: _loading
+                      ? Container(
+                          width: 350,
+                          child: Column(
+                            children: [
+                              Image.asset('assets/cat_dog_icon.png'),
+                              SizedBox(height: 10),
+                            ],
+                          ),
+                        )
+                      : Container(
+                          child: Column(
+                            children: [
+                              Container(
+                                height: 250,
+                                child: Image.file(_image),
+                              ),
+                              SizedBox(height: 20),
+                              _output != null
+                                  ? Text(
+                                      'Hurray !! We found out this is a ${_output[0]['label']}',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold),
+                                    )
+                                  : Container(),
+                              SizedBox(height: 100),
+                            ],
+                          ),
+                        ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          pickImage();
+                        },
+                        child: Container(
+                          width: MediaQuery.of(context).size.width - 150,
+                          alignment: Alignment.center,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 20),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(15)),
+                          child: Text(
+                            'Capture Image',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 12),
+                      GestureDetector(
+                        onTap: () {
+                          pickGalleryImage();
+                        },
+                        child: Container(
+                          width: MediaQuery.of(context).size.width - 150,
+                          alignment: Alignment.center,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 20),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(15)),
+                          child: Text(
+                            'Select an Image',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
+        ],
       ),
-      bottomNavigationBar: BottomAppBar(),
+      //bottomNavigationBar: BottomAppBar(),
     );
   }
 }
